237616a65814089372de4edcbe25663f58ccb399
Shahid <109452231+raza-shahi@users.noreply.github.com>
2025-12-04 15:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function shortcut(s1, s2) {
   // your code here
-	let s1 = s1.string;
-	let s2 = s2.string;
 	if(s1.length<1 || s2.length <1){
 		console.log('');
 		
@@ -11,6 +9,6 @@ function shortcut(s1, s2) {
 }
 
 // Do not change the code below.
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(shortcut(s1, s2));
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(shortcut(s1, s2));
